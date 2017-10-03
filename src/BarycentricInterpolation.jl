e48ae6240c88d2f8b41ef5faf5f6977ba39411c8
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2017-10-03 13:46:33 +0200

    discrete chebyshev for arbitrary nodes

--- src/BarycentricInterpolation.jl
+++ src/BarycentricInterpolation.jl
@@ -20,7 +20,7 @@ mutable struct InterpolationGrid{T<:Real}
             end
             w[k] = prod([s/(x[k]-x[j]) for j=1:k-1])
         end
-        new(x, w, s)
+        new(copy(x), w, s)
     end
 end
 InterpolationGrid(x::Array{T,1}) where T<:Real = InterpolationGrid{T}(x)
@@ -33,7 +33,7 @@ mutable struct InterpolationPolynomial{T<:Real}
         if length(f)!=length(g.x)
             error("wrong length of f")
         end
-        new(g,f)
+        new(g, copy(f))
     end
 end
 InterpolationPolynomial(g::InterpolationGrid{T}, f::Array{T,1}) where T<:Real = 
@@ -91,7 +91,8 @@ function change_point!{T<:Real}(p::InterpolationPolynomial{T}, index::Integer, x
 end
 
 chebyshev(d::Int, range::Tuple{Real, Real} = (-1.0,1.0)) = 
-    InterpolationPolynomial(range[1]+reverse(cos.((0:d)*pi/d)+1.0)*(range[2]-range[1])/2, (-1.0).^(d:-1:0))
+    ( d==0?InterpolationPolynomial([0.0],[1.0]):
+     InterpolationPolynomial(range[1]+reverse(cos.((0:d)*pi/d)+1.0)*(range[2]-range[1])/2, (-1.0).^(d:-1:0)) )
 
 function get_discrete_chebyshev_approx_nodes(n::Int, d::Int)
     m = ceil(Int,d/2)
@@ -115,7 +116,6 @@ end
 
 
 function discrete_chebyshev(n::Int, d::Int)
-    m = ceil(Int,d/2)
     x = get_discrete_chebyshev_approx_nodes(n,d)+0.0
     ll = length(x)
     f = -(-1.0).^(length(x):-1:1)
@@ -147,10 +147,36 @@ function discrete_chebyshev(n::Int, d::Int)
     p
 end
 
+
+function discrete_chebyshev(X::Array{Float64,1}, d::Int)
+    X = sort(X)
+    n = length(X)
+    nn = vcat(1:d-1,n)
+    ff = -(-1.0).^(length(nn):-1:1)
+    p = InterpolationPolynomial(X[nn],ff)
+    while true
+        y = [p(x) for x in  X]
+        k = k = findfirst(x->abs(x)>1+1000*eps(Float64), y)
+        if k==0
+            break
+        end
+        l1 = findfirst(p.g.x, X[k-1])
+        l2 = findfirst(p.g.x, X[k+1])
+        if l1>0 && sign(p.f[l1])==sign(y[k])
+            change_point!(p, l1, X[k])
+        elseif l2>0 && sign(p.f[l2])==sign(y[k])
+            change_point!(p, l2, X[k])   
+        else
+            error("something strange happened")
+        end    
+    end
+    p
+end
+
 function roots(p::InterpolationPolynomial{Float64})
     # This function implements a method given in 
     # Piers W. Lawrence, Robert M. Corless: Stability of rootfinding for 
-    # barycentric Lagrange interpolants,  R.M. Numer Algor (2014) 65: 447. 
+    # barycentric Lagrange interpolants, Numer. Algor. (2014) 65: 447. 
     # https://doi.org/10.1007/s11075-013-9770-3
     n = length(p.f)-1
     A = diagm(vcat(0.0,p.g.x))
@@ -162,7 +188,16 @@ function roots(p::InterpolationPolynomial{Float64})
     S2 = diagm(vcat(1.0/norm(A[1,:]),ones(n+1)))
     A = S1*A*S2
     B = diagm(vcat(0.0, ones(n+1)))
-    sort(eigvals(A,B))[1:end-2]
+    r = sort(eigvals(A,B))
+    if abs(r[1])==Inf && abs(r[2])==Inf
+        return r[3:end]
+    elseif abs(r[1])==Inf && abs(r[end])==Inf
+        return r[2:end-1]
+    elseif abs(r[end-1])==Inf && abs(r[end])==Inf    
+        return r[1:end-2]
+    else
+        error("something strange happened")
+    end
 end
 
 
